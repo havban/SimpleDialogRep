17d01cac97a6af2c0ae4940ac28f6bd526838982
havban <havban@gmail.com>
2010-10-21 15:45:46 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "oke");
+
+	}
+
+}
